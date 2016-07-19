7a4d706fea336489c34953f9b395b2c1429d580f
1amBulletproof <brandon.tarney@gmail.com>
2016-07-18 23:13:18 -0400

    format All code PEP 8 formatting

--- wheelofjeopardy/gui/jeopardyboardtkinter.py
+++ wheelofjeopardy/gui/jeopardyboardtkinter.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python27
-# Path to python
-
-# Written by Brandon Tarney
-# 12.20.14
-# program purpose: Create file manipulation classes...these classes will use REGEX, data structures & File reading/writing
+"""
+wheelofjeopardy GUI prototype using Tkinter
+"""
 
 from os import listdir 															# need package to find all csv files in given directory
 import csv 																		# need to read csv's
@@ -11,6 +8,9 @@ import csv 																		# need to read csv's
 import Tkinter														# for selecting the document to use
 from tkFileDialog import askopenfilename
 
+
+#@TODO REFORMAT THIS TO FIT PEP 8
+#@TODO Update this GUI to represent Game Board
 class FileComparisonGUI(Tkinter.Tk):
 	def __init__(self, parent=None):
 		Tkinter.Tk.__init__(self, parent)
@@ -28,21 +28,21 @@ class FileComparisonGUI(Tkinter.Tk):
 		self.SigLabel.grid(column = 2, row = 1, sticky = "N", padx = 20, pady = 10)
 
 		#buttons
-		self.ReqFileButton = Tkinter.Button(self, text = "Choose the Requirements File (CSV)", command = self.Choose_Requirements_File)
+		self.ReqFileButton = Tkinter.Button(self, text = "Choose the Requirements File (CSV)", command = self.choose_requirements_file)
 		self.ReqFileButton.grid(column = 0, row = 0, sticky = "S", padx = 20, pady = 10)
 
-		self.SigFileButton = Tkinter.Button(self, text = "Choose the Signals File (CSV)", command = self.Choose_Signals_File)
+		self.SigFileButton = Tkinter.Button(self, text = "Choose the Signals File (CSV)", command = self.choose_signals_file)
 		self.SigFileButton.grid(column = 2, row = 0, sticky = "S", padx = 20, pady = 10)
 
 		self.QuitButton = Tkinter.Button(self, text = "QUIT", command = self.quit)
 		self.QuitButton.grid(column = 1, row = 4, padx = 30, pady = 15)
 
-	def Choose_Requirements_File(self):
+	def choose_requirements_file(self):
 		name = askopenfilename()
 		name_string = str(name)
 		self.ReqLabel["text"] = name_string
 
-	def Choose_Signals_File(self):
+	def choose_signals_file(self):
 		name = askopenfilename()
 		name_string = str(name)
 		self.SigLabel["text"] = name_string
