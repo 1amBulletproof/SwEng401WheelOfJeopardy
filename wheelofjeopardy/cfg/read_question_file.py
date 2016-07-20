2494e657d3575b613a615ff89ba2e5a8e87fa9bc
J Wu <johnwuy@gmail.com>
2016-07-20 10:50:40 -0400

    Take out code used for debugging

--- wheelofjeopardy/cfg/read_question_file.py
+++ wheelofjeopardy/cfg/read_question_file.py
@@ -40,9 +40,8 @@ def getDataFromQRow(row):
 
 #%% Parse CSV line-by-line and instantiate the various questions
 
-# Preallocate lists for now
-catgRd1 = dict() # dict to keep list index for round 1 categories
-catgRd2 = dict() # dict to keep list index for round 2 categories
+catgRd1 = dict() # dict to keep list indices for round 1 categories
+catgRd2 = dict() # dict to keep list indices for round 2 categories
 # the following 2D-list are to store questions for round 1 and 2
 qsRd1 = [[None for x in range(numQs)] for y in range(numCats)]
 qsRd2 = [[None for x in range(numQs)] for y in range(numCats)]
@@ -51,28 +50,24 @@ with open(fPath, 'r') as csvfile:
     reader = csv.DictReader(csvfile)
 
     for row in reader:
-        try:
-            (rdInd, cat, pts, qTxt, ans, dd) = getDataFromQRow(row) # read row
-        except:
-            print('oh oh')
-            pass
+        (rdInd, cat, pts, qTxt, ans, dd) = getDataFromQRow(row) # read row
 
         if rdInd == 1:
             if not catgRd1.has_key(cat): # if category not encountered yet
                 catgRd1[cat] = len(catgRd1) # designate new index
+
             # put question class instantiation here
-            try:
-                qsRd1[catgRd1[cat]][pts-1] = qTxt +'|' +ans # placeholder for now
-            except:
-                print('no')
-                pass
+            qsRd1[catgRd1[cat]][pts-1] = qTxt +'|' +ans # placeholder for now
+
         elif rdInd == 2:
             if not catgRd2.has_key(cat):
                 catgRd2[cat] = len(catgRd2)
+
             # put question class instantiation here
             qsRd2[catgRd2[cat]][pts-1] = qTxt +'|' +ans # placeholder for now
-        else:
-            pass # do nothing with final jeopardy for now
+
+        else: # do nothing with final jeopardy for now
+            pass
 
 #%% Instantiate QuestionMatrix method, check for consistency, and return
 
