dc79324844d2c190216741995b1bfe24f37c9209
Cameron Dutro <camertron@gmail.com>
2016-07-21 10:31:44 -0700

    Adding event broadcast tests to player state

--- tests/player_state_tests.py
+++ tests/player_state_tests.py
@@ -1,12 +1,12 @@
 import unittest
 
-from wheelofjeopardy.events import Events
+from tests.support import TestEvents
 from wheelofjeopardy.player_state import PlayerState, NoTokensAvailableError
 
 class PlayerStateTestCase(unittest.TestCase):
     def setUp(self):
-        events = Events()
-        self.player_state = PlayerState(name='Monty', events=events)
+        self.events = TestEvents()
+        self.player_state = PlayerState(name='Monty', events=self.events)
 
 class TestInitialPlayerState(PlayerStateTestCase):
     def test_name_attribute(self):
@@ -19,19 +19,23 @@ class TestInitialPlayerState(PlayerStateTestCase):
         self.assertEqual(self.player_state.free_spin_tokens, 0)
 
 class TestScoreControls(PlayerStateTestCase):
-    def setUp(self):
-        events = Events()
-        self.player_state = PlayerState(name='Monty', events=events)
-
     def test_increases_score(self):
         self.assertEqual(self.player_state.score, 0)
         self.player_state.increase_score_by(10)
         self.assertEqual(self.player_state.score, 10)
 
+    def test_score_increase_fires_event(self):
+        self.player_state.increase_score_by(10)
+        self.assertTrue(self.events.did_broadcast('player_state.score_did_update'))
+
     def test_decreases_score(self):
         self.assertEqual(self.player_state.score, 0)
         self.player_state.decrease_score_by(10)
         self.assertEqual(self.player_state.score, -10)
+
+    def test_score_decrease_fires_event(self):
+        self.player_state.decrease_score_by(10)
+        self.assertTrue(self.events.did_broadcast('player_state.score_did_update'))
 
 class TestFreeSpinTokenControls(PlayerStateTestCase):
     def test_increases_tokens(self):
@@ -39,11 +43,20 @@ class TestFreeSpinTokenControls(PlayerStateTestCase):
         self.player_state.grant_free_spin_token()
         self.assertEqual(self.player_state.free_spin_tokens, 1)
 
+    def test_token_increase_fires_event(self):
+        self.player_state.grant_free_spin_token()
+        self.assertTrue(self.events.did_broadcast('player_state.spin_tokens_did_update'))
+
     def test_decreases_tokens(self):
         self.player_state.grant_free_spin_token()
         self.assertEqual(self.player_state.free_spin_tokens, 1)
         self.player_state.use_free_spin_token()
         self.assertEqual(self.player_state.free_spin_tokens, 0)
+
+    def test_token_decrease_fires_event(self):
+        self.player_state.free_spin_tokens = 1
+        self.player_state.use_free_spin_token()
+        self.assertTrue(self.events.did_broadcast('player_state.spin_tokens_did_update'))
 
     def test_raises_error_when_no_tokens_available(self):
         self.assertEqual(self.player_state.free_spin_tokens, 0)
