30daef6f7b898a2edde542cf8b3c1e69a0ddc4e4
1amBulletproof <brandon.tarney@gmail.com>
2016-07-21 18:39:13 -0400

    Update to prepare wheel/sector classes for collaboration

--- wheelofjeopardy/wheel.py
+++ wheelofjeopardy/wheel.py
@@ -8,10 +8,12 @@ from wheelofjeopardy.sectors import *
 class Wheel(object):
     #@TODO: initialization incomplete?
     def __init__(self):
+        pass
         # self._sectors = self._initialize_sectors()
 
     #@TODO: update sectors for actual physical implementations available
     def _initialize_sectors(self):
+        pass
         # sector1 = Section1()
         # sector2 =
         # sector3 =
@@ -28,10 +30,13 @@ class Wheel(object):
     get random sector, notify GUI of this sector (to show/animate it), and pass game_state to the random sector's action() method
     """
     #@TODO:unfinished method
-    def spin(self, game_state, gui):
+    def spin(self):
+        pass
         # self.current_sector = self._get_random_sector()
         # #Notify GUI? GUI Observers?
-        # self.current_sector.action(game_state)
+        # self.current_sector.action(game_state) - handled by the game_state currently
+        # return self.current_sector
 
     #@TODO: get random number and return that index of self._sectors
     def _get_random_sector(self):
+        pass
