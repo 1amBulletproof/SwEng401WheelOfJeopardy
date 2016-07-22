1232e82f13f4b61dd797313767192a1826e6609e
1amBulletproof <brandon.tarney@gmail.com>
2016-07-21 21:37:05 -0400

    fix bad merge

--- wheelofjeopardy/sectors/sector.py
+++ wheelofjeopardy/sectors/sector.py
@@ -2,18 +2,11 @@
 Sector Super Class
 """
 
-<<<<<<< HEAD
 #@TODO: unit tests for class
 class Sector:
     def __init__(self, name):
         self.name = name
 
-    def action(self):
-        raise NotImplementedError()
-=======
-# placeholder
-class Sector(object):
-    def apply_to(self, game_state):
+    def action(self, game_state):
         game_state.end_turn()
         pass
->>>>>>> dc79324844d2c190216741995b1bfe24f37c9209
