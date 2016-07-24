57e07248d72eb1e777769d79c909f1158c385646
vscalfari <vscalfari@yahoo.com>
2016-07-24 10:56:19 -0400

    cleaned up methods and made QuestionMatrix and QuestionBoardState align with the class diagram

--- wheelofjeopardy/question_board_state.py
+++ wheelofjeopardy/question_board_state.py
@@ -8,14 +8,20 @@ which questions have already been selected.
 class QuestionBoardState(object):
     def __init__(self, events):
         self.events = events
-
-        # @TODO: replace with correct logic
         self.questions_remaining = 30
+        self.questions = QuestionMatrix()
+        #self.currentQuestion = 
+        #self.category_statuses -not sure what this is for
 
     def any_questions_remaining(self):
-        # @TODO: replace with correct logic
+         @TODO: replace with correct logic
         return self.questions_remaining > 0
 
     def mark_question_used(self):
         if any_questions_remaining():
             self.questions_remaining = self.questions_remaining - 1
+            #insert a way of knowing this question is used?
+
+    def next_in_category(category):
+         #@TODO: replace with correct logic
+        
