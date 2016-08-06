e1d81b68afb61a6da8f177f9058cadc83d6fb2bc
1amBulletproof <brandon.tarney@gmail.com>
2016-08-06 17:09:11 -0400

    update wheel class, finish constructor & get_random_sector

--- wheelofjeopardy/wheel.py
+++ wheelofjeopardy/wheel.py
@@ -1,16 +1,14 @@
 """
 Wraps the sector classes and controls which affect the game_state
 """
-#FARHEEN COMMENT
+
 from wheelofjeopardy.sectors.sector import *
 
 #@TODO: create unit tests for this class
 class Wheel(object):
-    #@TODO: initialization incomplete?
     def __init__(self, event):
         self.description = 'Wheel from the game Wheel of Jeopardy!'
-        pass
-        # self._sectors = self._initialize_sectors()
+        self._sectors = self._initialize_sectors()
 
     #@TODO: update sectors for actual physical implementations available
     def _initialize_sectors(self):
@@ -28,21 +26,12 @@ class Wheel(object):
         # return [sector1, sector2, sector3, sector4, sector5, sector6 ... sector12]
 
     """
-    get random sector, notify GUI of this sector (to show/animate it), and return the random sector
+    get random sector, notify GUI of this sector (to show/animate it)
+    and return the random sector
     """
-    #@TODO:unfinished method
     def get_random_sector(self):
-        self.sector_selected = Sector("random sector")
-        return self.sector_selected
-        # self.current_sector = self._get_random_sector()
-        # #Notify GUI? GUI Observers?
-        # self.current_sector.action(game_state) - handled by the game_state currently
-        # return self.current_sector
-
-    #@TODO: get random number and return that index of self._sectors
-    def _get_random_sector(self):
-        pass
-
+        random_number = random.randrange(0, 11, 1)
+        return self._sectors[random_number]
 
     def __str__(self):
         return self.description
