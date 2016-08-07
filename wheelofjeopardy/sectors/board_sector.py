31cb06e27ae4e0068ff8f103c26062377dc66acb
Cameron Dutro <camertron@gmail.com>
2016-08-07 15:55:56 -0700

    BoardSector should ask game state for next question, then broadcast

--- wheelofjeopardy/sectors/board_sector.py
+++ wheelofjeopardy/sectors/board_sector.py
@@ -6,9 +6,10 @@ from wheelofjeopardy.sectors.sector import Sector
 
 class BoardSector(Sector):
     def __init__ (self, category):
-        Sector.__init__(self, str(category)+" Board sector")
+        Sector.__init__(self, str(category) + " Board sector")
         self.category = category
 
-    #@TODO Apply Question Selection logic (i.e. are there any questions left: see sequence diagram)
     def action(self, game_state):
-        game_state.set_category(self.category)
+        game_state.current_category = self.category
+        game_state.current_question = game_state.next_question_in_category(self.category)
+        game_state.events.broadcast('board_sector.question_will_be_asked', game_state.current_question)
