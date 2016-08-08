1e243e76758208e92285d410907a4a48abaa7be3
Miranda Link <mirandanlink@gmail.com>
2016-08-07 22:03:20 -0400

    miranda still catching up

--- wheelofjeopardy/gui/moderator_popup.py
+++ wheelofjeopardy/gui/moderator_popup.py
@@ -1,8 +1,48 @@
-from os import sys
-from PyQt4.QtGui import QMainWindow
-from wheelofjeopardy.gui.pyqt.ui_moderator_popup import Ui_ModeratorPopup
+'''
+This is an internal class called when "submit answer" button is clicked.
 
-class ModeratorPopup(QMainWindow, Ui_ModeratorPopup):
-  def __init__(self, parent=None):
+@author = Miranda Link, mirandanlink@gmail.com
+'''
+
+from os import sys
+from PyQt4.QtGui import *
+from wheelofjeopardy.gui.pyqt.ui_moderator_popup import Ui_ModeratorPopup
+from wheelofjeopardy.events import Events
+
+
+class ModeratorPopup(QDialog, Ui_ModeratorPopup):
+  def __init__(self, events, parent=None):
     super(ModeratorPopup, self).__init__(parent)
     self.setupUi(self)
+    self.events = events
+    
+    # initialize variables
+    #
+    #self.correctAnswer.setText(correct_answer)
+    #self.playerAnswer.setText(player_answer)
+
+  # button clicks
+  #
+  #@pyqtSignature("")
+  def _on_playerCorrectRadio_clicked(self):
+    self.events.broadcast('gui.correct_answer_received')
+    self.events.broadcast('moderator.update_score')
+
+  #@pyqtSignature("")
+  def _on_playerIncorrectRadio_clicked(self):
+    self.events.broadcast('gui.incorrect_answer_received')
+
+
+#Display GUI.
+if __name__ == '__main__':
+  application = QApplication(sys.argv)
+  application.setApplicationName("Moderator Popup")
+  events = Events()
+  gui = ModeratorPopup(events)
+  gui.show()
+  gui.raise_()
+  application.exec_()
+
+# subscriptions
+#
+events.subscribe('gui.answer_received', ModeratorPopup)
--- wheelofjeopardy/gui/text_gui.py
+++ wheelofjeopardy/gui/text_gui.py
@@ -31,12 +31,8 @@ class TextGUI(object):
 
     @staticmethod
     def _clear_terminal():
+        raw_input("Press Enter to continue...")
         os.system('cls' if os.name=='nt' else 'clear')
-
-    # not sure if the following is needed anymore??
-#	@classmethod
-#	def _create_player(cls, name, events, score):
-#		return PlayerState(name=name, events=events, score=score)
 
     # public instance
 
@@ -50,6 +46,10 @@ class TextGUI(object):
         self.events.subscribe('game_state.current_player_did_change', self._on_current_player_did_change)
         self.events.subscribe('game_state.spins_did_update', self._on_spins_did_update)
         self.events.subscribe('game_state.turn_will_end', self._on_turn_will_end)
+        self.events.subscribe('game_state.sector_was_chosen', self._on_sector_was_chosen)
+
+        self.events.subscribe('board_sector.question_will_be_asked', self._on_question_will_be_asked)
+        self.events.subscribe('board_sector.check_answer', self._on_check_answer)
 
         TextGUI._clear_terminal()
         while self.game_state.any_spins_remaining():
@@ -71,21 +71,43 @@ class TextGUI(object):
         print self._get_whose_turn_message()
 
     def _on_spins_did_update(self, game_state):
-        print('Your spinned %s.' % str(self.game_state.current_sector))
         print self._get_spins_remaining_message()
+
+    def _on_sector_was_chosen(self, sector):
+        print('Your spinned %s.' % str(sector))
+        TextGUI._clear_terminal()
 
     def _on_turn_will_end(self, game_state):
         print 'That concludes %s turn.' % apostrophize(game_state.get_current_player().name)
 
+    def _on_question_will_be_asked(self, question):
+        sys.stdout.write('%s: ' % (question[2].text))
+        answer = raw_input()
+        self.events.broadcast('gui.answer_received', answer)
+
+    def _on_check_answer(self, question, answer):
+        mod_response = ''
+
+        while mod_response != 'y' and mod_response != 'n':
+            player_name = self.game_state.get_current_player().name
+            print 'Correct answer is: %s' % (question[2].answer)
+            sys.stdout.write('Hey moderator, is %s answer correct (y/n)? ' % (apostrophize(player_name)))
+            mod_response = raw_input()
+
+        if mod_response == 'y':
+            self.events.broadcast('gui.correct_answer_received', question)
+        else:
+            self.events.broadcast('gui.incorrect_answer_received', question)
+
     def _print_scores(self):
         score_strings = []
 
-        for player_state in self.game_state.player_states:
-            score_strings.append("\t%s has %d points" % (player_state.name, player_state.score))
+        for pl in self.game_state.player_states:
+            score_strings.append("\t%s has %u points, %u tokens." % \
+                                 (pl.name, pl.score, pl.free_spin_tokens) )
 
         print 'Here are the scores:'
         print '\n'.join(score_strings)
-        raw_input("Press Enter to continue...")
         TextGUI._clear_terminal()
 
     def _get_spins_remaining_message(self):
