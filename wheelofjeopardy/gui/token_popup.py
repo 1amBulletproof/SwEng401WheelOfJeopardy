2490aa9ae16196b6bf863ac6534b1be1edd52c4d
Miranda Link <mirandanlink@gmail.com>
2016-08-07 23:19:30 -0400

    update to gui classes, all should pop up now except daily double

--- wheelofjeopardy/gui/token_popup.py
+++ wheelofjeopardy/gui/token_popup.py
@@ -1,8 +1,42 @@
-from os import sys
-from PyQt4.QtGui import QMainWindow
-from wheelofjeopardy.gui.pyqt.ui_token_popup import Ui_TokenPopup
+'''
+This is called to have a player accept or deny token use.
 
-class TokenPopup(QMainWindow, Ui_TokenPopup):
-  def __init__(self, parent=None):
+@author = Miranda Link, mirandanlink@gmail.com
+'''
+
+from os import sys
+from PyQt4.QtGui import *
+from wheelofjeopardy.gui.pyqt.ui_token_popup import Ui_TokenPopup
+from wheelofjeopardy.events import Events
+
+class TokenPopup(QDialog, Ui_TokenPopup):
+  def __init__(self, events, parent=None):
     super(TokenPopup, self).__init__(parent)
     self.setupUi(self)
+    self.events = events
+
+    # initialize variables
+    #
+
+    # button clicks
+    #
+    def _on_useTokenButton_clicked():
+      self.events.broadcast('gui.use_free_token')
+
+    def _on_denyTokenButton_clicked():
+      self.events.broadcast('gui.dont_use_free_token')
+
+
+# Display GUI.
+#
+if __name__ == '__main__':
+  application = QApplication(sys.argv)
+  application.setApplicationName("Token Popup")
+  events = Events()
+  gui = TokenPopup(events)
+  gui.show()
+  gui.raise_()
+  application.exec_()
+
+# subscriptions
+events.subscribe('gui.show_token_popup', TokenPopup)
