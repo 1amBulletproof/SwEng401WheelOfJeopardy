26e27d4ddc9bdd0b37c7dd0fc0cc3a3368ff288c
Cameron Dutro <camertron@gmail.com>
2016-08-10 09:11:20 -0700

    Add event logging env option.

--- wheelofjeopardy/events.py
+++ wheelofjeopardy/events.py
@@ -8,18 +8,27 @@ When an event is broadcast, all the subscribers of that event
 receive the message and any included arguments.
 """
 
+import os
+
 class Events(object):
-  def __init__(self):
-    self.subscriptions = {}
+    def __init__(self):
+        self.subscriptions = {}
 
-  def subscribe(self, channel, func):
-    if channel in self.subscriptions:
-      self.subscriptions[channel].append(func)
-    else:
-      self.subscriptions[channel] = [func]
+    def subscribe(self, channel, func):
+        if channel in self.subscriptions:
+            self.subscriptions[channel].append(func)
+        else:
+            self.subscriptions[channel] = [func]
 
-  def broadcast(self, channel, *args):
-    print(channel)
-    if channel in self.subscriptions:
-      for func in self.subscriptions[channel]:
-        func(*args)
+    def broadcast(self, channel, *args):
+        if self._log_events():
+            print(channel)
+
+        if channel in self.subscriptions:
+            for func in self.subscriptions[channel]:
+                func(*args)
+
+    # private
+
+    def _log_events(self):
+        return os.getenv('LOG_EVENTS', 'false').lower() == 'true'
